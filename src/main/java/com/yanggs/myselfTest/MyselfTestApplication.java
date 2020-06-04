2b628fc44eae072ecc962f5f658f35ad9f3c8bfa
yangguoshuai <1173057075@qq.com>
2020-06-04 23:47:04 +0800

    测试

---
+++ src/main/java/com/yanggs/myselfTest/MyselfTestApplication.java
@@ -0,0 +1,13 @@
+package com.yanggs.myselfTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyselfTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyselfTestApplication.class, args);
+	}
+
+}
